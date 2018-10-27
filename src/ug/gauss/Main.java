2fd792b2cdd6f97084a1e5a12be2cf8556360ad6
Mateusz Małecki <mmateusz314@gmail.com>
2018-10-28 00:38:42 +0200

    Base

--- src/ug/gauss/Main.java
+++ src/ug/gauss/Main.java
@@ -1,10 +1,22 @@
 package ug.gauss;
 
+import java.util.Random;
 
+import ug.gauss.datatypes.FractionComp;
 
 public class Main {
 
+    static int randomNumber(int seed)
+    {
+        Random random = new Random();
+        return random.ints(-65536,(65536)).findFirst().getAsInt();
+    }
+
+
     public static void main(String[] args) {
-	// write your code here
+
+        MyMatrix<FractionComp> mm = new MyMatrix<FractionComp>();
+        System.out.println(Main.randomNumber(1));
+
     }
 }
