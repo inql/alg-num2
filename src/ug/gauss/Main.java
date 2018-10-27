a12f761ff3b88170e9570287407653fd8f6290cf
Mateusz Małecki <mmateusz314@gmail.com>
2018-10-28 00:25:52 +0200

    Fixed Main

--- src/ug/gauss/Main.java
+++ src/ug/gauss/Main.java
@@ -1,16 +1,10 @@
 package ug.gauss;
 
-<<<<<<< HEAD
 
-public class Main {
 
-    public static void main(String[] args) {
-
-=======
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
->>>>>>> 9d8f852d1db32ba87990e0d2de0408e9db767492
     }
 }
